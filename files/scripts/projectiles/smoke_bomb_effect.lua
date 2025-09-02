a67e14e5550ce202a57d1c350826767d9f8142c2
Daniel Meyer <dansebmey@gmail.com>
2025-09-02 15:12:40 +0200

    change borrowed time tooltip

--- files/scripts/projectiles/smoke_bomb_effect.lua
+++ files/scripts/projectiles/smoke_bomb_effect.lua
@@ -7,17 +7,16 @@ local owner = EntityGetParent( entity_id )
 local player = get_player()
 local x, y = EntityGetTransform( entity_id )
 
--- local mortals = EntityGetInRadiusWithTag( x, y, EFFECT_RADIUS, "mortal" )
--- for i,mortal in ipairs(mortals) do
--- 	GamePrint("mortal found!")
--- 	LoadGameEffectEntityTo( mortal, "data/entities/misc/effect_invisibility_short.xml" )
--- end
+local mortals = EntityGetInRadiusWithTag( x, y, EFFECT_RADIUS, "mortal" )
+for i,mortal in ipairs(mortals) do
+	LoadGameEffectEntityTo( mortal, "data/entities/misc/effect_invisibility_short.xml" )
+end
 
-local matconv_id = EntityAddComponent2( entity_id, "MagicConvertMaterialComponent", 
-{
-	from_material = CellFactory_GetType( "air" ),
-	to_material = CellFactory_GetType( "smoke_invisibility" ),
-	is_circle = true,
-	radius = EFFECT_RADIUS,
-	kill_when_finished = true,
-} )
+-- local matconv_id = EntityAddComponent2( entity_id, "MagicConvertMaterialComponent", 
+-- {
+-- 	from_material = CellFactory_GetType( "air" ),
+-- 	to_material = CellFactory_GetType( "smoke_invisibility" ),
+-- 	is_circle = true,
+-- 	radius = EFFECT_RADIUS,
+-- 	kill_when_finished = true,
+-- } )
