008cca10e5c2dc6025c7d3f61a7b012bf84ade96
Daniel Meyer <dansebmey@gmail.com>
2025-09-13 00:28:24 +0200

    simplify master of fire

--- files/scripts/perks/effect_master_of_fire_update_slow.lua
+++ files/scripts/perks/effect_master_of_fire_update_slow.lua
@@ -30,32 +30,18 @@ for i,enemy in ipairs(enemies) do
         -- custom fire damage formula
         local remaining_hp_dmg = ( e_hp - e_max_hp * 0.2 ) * 0.02
 
-        -- effectively set regular fire damage to 0 for the player
         if ( enemy == owner ) then
-            EntityInflictDamage( owner, -(e_max_hp * 0.01), "DAMAGE_HEALING", "master_of_fire healing", "NONE", 0, 0, owner, x, y, 0 )
-
             -- fire heals the player if their health is below 20%
             if ( p_hp < p_max_hp * 0.2 ) then
-                local heal_dmg = -1 * p_max_hp * 0.01
-                EntityInflictDamage( owner, heal_dmg, "DAMAGE_HEALING", "master_of_fire healing", "NONE", 0, 0, owner, x, y, 0 )
+                local heal_dmg = -1 * ( p_max_hp * 0.02 )
+                EntityInflictDamage( owner, heal_dmg, "DAMAGE_HEALING", "master of fire healing", "NONE", 0, 0, owner, x, y, 0 )
+
                 GamePlaySound( "data/audio/Desktop/misc.bank", "game_effect/regeneration/tick", x, y )
             end
-
-            -- if the player is burning, twice the remaining health-based fire damage spreads to nearby enemies
-            for i,other_enemy in ipairs(enemies) do
-                if ( other_enemy ~= owner ) then
-                    EntityInflictDamage( other_enemy, remaining_hp_dmg * 2, "DAMAGE_FIRE", "master_of_fire fire", "NONE", 0, 0, owner, x, y, 0 )
-                    
-                    local e_x, e_y = EntityGetTransform( other_enemy )
-                    if ( not has_game_effect( other_enemy, "ON_FIRE" ) ) then
-                        EntityLoad( "mods/D2DContentPack/files/entities/projectiles/deck/hitfx_master_of_fire_impact.xml", e_x, e_y )
-                    end
-                end
-            end
+        else
+            -- apply the custom fire damage formula
+            EntityInflictDamage( enemy, remaining_hp_dmg, "DAMAGE_FIRE", "master_of_fire fire", "NONE", 0, 0, owner, x, y, 0 )
         end
-
-        -- apply the custom fire damage formula
-        EntityInflictDamage( enemy, remaining_hp_dmg, "DAMAGE_FIRE", "master_of_fire fire", "NONE", 0, 0, owner, x, y, 0 )
 
         -- nearby burning enemies heal the player while health is below 40%
         -- if ( enemy ~= owner and p_hp < p_max_hp * 0.4 and distance_between( owner, enemy ) < EFFECT_RADIUS_HEALING ) then
