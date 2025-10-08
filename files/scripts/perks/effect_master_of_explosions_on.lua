4d4ecb13393448189437ecf77e4f1a209fe40f5b
Daniel Meyer <dansebmey@gmail.com>
2025-10-08 13:08:54 +0200

    disable perk removal on "master" perks (just in case)

--- files/scripts/perks/effect_master_of_explosions_on.lua
+++ files/scripts/perks/effect_master_of_explosions_on.lua
@@ -3,16 +3,16 @@ dofile_once("data/scripts/lib/utilities.lua")
 local entity_id = GetUpdatedEntityID()
 local owner = EntityGetRootEntity( entity_id )
 
-local is_immune_to_explosions = has_game_effect( owner, "PROTECTION_EXPLOSION" )
-if( is_immune_to_explosions ) then
-	if( GameHasFlagRun( "PERK_PICKED_PROTECTION_EXPLOSION" ) ) then
-	   remove_perk( "PROTECTION_EXPLOSION" )
-	end
+-- local is_immune_to_explosions = has_game_effect( owner, "PROTECTION_EXPLOSION" )
+-- if( is_immune_to_explosions ) then
+-- 	if( GameHasFlagRun( "PERK_PICKED_PROTECTION_EXPLOSION" ) ) then
+-- 	   remove_perk( "PROTECTION_EXPLOSION" )
+-- 	end
 
-	if( GameHasFlagRun( "PERK_PICKED_EXPLODING_CORPSES" ) ) then
-	   remove_perk( "EXPLODING_CORPSES" )
-	end
-end
+-- 	if( GameHasFlagRun( "PERK_PICKED_EXPLODING_CORPSES" ) ) then
+-- 	   remove_perk( "EXPLODING_CORPSES" )
+-- 	end
+-- end
 
 
 
--- files/scripts/perks/effect_master_of_fire_on.lua
+++ files/scripts/perks/effect_master_of_fire_on.lua
@@ -3,20 +3,20 @@ dofile_once( "data/scripts/lib/utilities.lua" )
 local entity_id = GetUpdatedEntityID()
 local owner = EntityGetParent( entity_id )
 
-local is_immune_to_fire = has_game_effect( owner, "PROTECTION_FIRE" )
-if ( is_immune_to_fire ) then
-   if ( GameHasFlagRun( "PERK_PICKED_PROTECTION_FIRE" ) ) then
-   	remove_perk( "PROTECTION_FIRE" )
-   end
+-- local is_immune_to_fire = has_game_effect( owner, "PROTECTION_FIRE" )
+-- if ( is_immune_to_fire ) then
+--    if ( GameHasFlagRun( "PERK_PICKED_PROTECTION_FIRE" ) ) then
+--    	remove_perk( "PROTECTION_FIRE" )
+--    end
 
-   if ( GameHasFlagRun( " PERK_PICKED_BLEED_OIL" ) ) then
-   	remove_perk( "BLEED_OIL" )
-   end
+--    if ( GameHasFlagRun( " PERK_PICKED_BLEED_OIL" ) ) then
+--    	remove_perk( "BLEED_OIL" )
+--    end
 
-   if ( GameHasFlagRun( " PERK_PICKED_FREEZE_FIELD" ) ) then
-   	remove_perk( "FREEZE_FIELD" )
-   end
-end
+--    if ( GameHasFlagRun( " PERK_PICKED_FREEZE_FIELD" ) ) then
+--    	remove_perk( "FREEZE_FIELD" )
+--    end
+-- end
 
 addNewInternalVariable( owner, "master_of_fire_is_effect_active", "value_int", 0 )
 addNewInternalVariable( owner, "master_of_fire_shot_effect_id", "value_int", 0 )
