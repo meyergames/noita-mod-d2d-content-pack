ed97d343d15273d277ad4dce6004b0df79af1652
Daniel Meyer <dansebmey@gmail.com>
2025-10-17 22:32:16 +0200

    evolving wands: get 1st wand if no held wand

--- files/scripts/perks/effect_evolving_wands_update.lua
+++ files/scripts/perks/effect_evolving_wands_update.lua
@@ -15,11 +15,35 @@ if ( GameHasFlagRun( flag ) == false ) then
 	local EZWand = dofile_once("mods/D2DContentPack/files/scripts/lib/ezwand.lua")
 	local wand = EZWand.GetHeldWand()
 
-    if wand then
-    	dofile_once( "mods/D2DContentPack/files/scripts/wand_utils.lua" )
-    	apply_random_wand_upgrades( wand, get_perk_pickup_count( "D2D_EVOLVING_WANDS" ) )
-
+	if wand then -- first try to upgrade the player's held wand
+		dofile_once( "mods/D2DContentPack/files/scripts/wand_utils.lua" )
+		apply_random_wand_upgrades( wand, get_perk_pickup_count( "D2D_WANDSMITH" ), "held wand" )
+		
 		GamePlaySound( "data/audio/Desktop/misc.bank", "game_effect/regeneration/tick", x, y )
 		GameAddFlagRun( flag )
-    end
+	else -- if no wand is held, get the first wand
+		local children = EntityGetAllChildren( player_id )
+		for k=1,#children do
+			child = children[k]
+		    if EntityGetName( child ) == "inventory_quick" then
+		        local inventory_items = EntityGetAllChildren(child)
+		        if inventory_items ~= nil then
+		            for z=1, #inventory_items do
+		            	item = inventory_items[z]
+
+		            	if EZWand.IsWand( item ) then
+		            		local wand = EZWand( item )
+					    	dofile_once( "mods/D2DContentPack/files/scripts/wand_utils.lua" )
+					    	apply_random_wand_upgrades( wand, get_perk_pickup_count( "D2D_EVOLVING_WANDS" ), "first wand" )
+
+							GamePlaySound( "data/audio/Desktop/misc.bank", "game_effect/regeneration/tick", x, y )
+							GameAddFlagRun( flag )
+
+		            		break
+		            	end
+		            end
+		        end
+		    end
+		end
+	end
 end
