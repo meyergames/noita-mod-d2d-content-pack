eb244a8d82d19a2d34b14cc44cbf89403e6a271c
Daniel Meyer <dansebmey@gmail.com>
2025-11-25 21:20:53 +0100

    make Feline Affection cat fleeing more consistent

--- files/scripts/animals/cat_damage_incoming.lua
+++ files/scripts/animals/cat_damage_incoming.lua
@@ -2,13 +2,13 @@ dofile_once( "data/scripts/lib/utilities.lua" )
 
 local entity_id = GetUpdatedEntityID()
 local owner = entity_id
-local x, y = EntityGetTransform( owner )
+local cx, cy = EntityGetTransform( owner )
 
 function damage_about_to_be_received( damage, x, y, entity_thats_responsible, critical_hit_chance )
     if entity_thats_responsible == get_player() then
 		-- if the damage comes from the player, "teleport" the cat away
-		EntityLoad( "mods/D2DContentPack/files/particles/tele_particles.xml", x, y )
-		EntityKill( owner )
+		EntityLoad( "mods/D2DContentPack/files/particles/tele_particles.xml", cx, cy )
+		EntityAddComponent2( owner, "LifetimeComponent", { lifetime = 1 } )
 		GamePrint( "You scared the cat away... :(" )
         return 0, critical_hit_chance
     elseif damage > 0 then
