c3b6c957fbeee005e2297129022076a9a428c3da
Daniel Meyer <dansebmey@gmail.com>
2025-11-25 21:20:30 +0100

    make Blink "never unlimited"

--- settings.lua
+++ settings.lua
@@ -6,7 +6,7 @@ mod_settings =
 {
     {
         category_id = "default_settings",
-        ui_name = "Build version: 25.11.02.1",
+        ui_name = "Build version: 25.11.25.1",
         ui_description = "",
         settings = {
         }
