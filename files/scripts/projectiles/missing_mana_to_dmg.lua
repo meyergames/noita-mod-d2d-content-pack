a85c4e8ba38f8d6906149a4b1a0b9657a66198e0
Daniel Meyer <dansebmey@gmail.com>
2026-01-05 21:26:55 +0100

    make MMTD stronger on high-lvl wands

--- files/scripts/projectiles/missing_mana_to_dmg.lua
+++ files/scripts/projectiles/missing_mana_to_dmg.lua
@@ -4,14 +4,20 @@ local wand = EZWand.GetHeldWand()
 
 local proj_id = GetUpdatedEntityID()
 local proj_comp = EntityGetFirstComponent( proj_id, "ProjectileComponent" )
-if exists( proj_comp ) then
+local was_already_applied = get_internal_bool( proj_id, "d2d_missing_mana_to_dmg_applied" )
+if exists( proj_comp ) and not was_already_applied then
 	-- multiply by up to x4 based on the projectile's mana cost (i.e. magic missile gets more mtp than spark bolt)
 	local proj_mana_cost = ComponentObjectGetValue2( proj_comp, "config", "action_mana_drain" )
 	local mana_cost_mtp = remap( proj_mana_cost, 5, 120, 1.0, 4.0 )
 
 	local missing_mana = wand.manaMax - wand.mana
 	local time_until_full_mana = missing_mana / math.max( wand.manaChargeSpeed, 10 )
-    local mtp = 1.0 + ( ( time_until_full_mana * 0.1 ) * mana_cost_mtp )
+	local mtp1 = ( time_until_full_mana * 0.05 ) -- so 60s = x3
+	local mtp2 = remap( proj_mana_cost, 5, 120, 1.0, 4.0 )
+	local mtp3 = remap( missing_mana, 50, 5000, 1.0, 4.0 )
+
+    local mtp = 1.0 + ( mtp1 * mtp2 * mtp3 )
     
 	multiply_proj_dmg( proj_id, mtp )
+	set_internal_bool( proj_id, "d2d_missing_mana_to_dmg_applied", true )
 end
