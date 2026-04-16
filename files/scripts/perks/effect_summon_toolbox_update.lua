9306d2c5e2e513b31bca3e5fb1d39acec1fdcfaf
Daniel Meyer <dansebmey@gmail.com>
2026-04-16 22:54:03 +0200

    make Toolbox spawn an upgrade on every HM

--- files/scripts/perks/effect_summon_toolbox_update.lua
+++ files/scripts/perks/effect_summon_toolbox_update.lua
@@ -4,7 +4,15 @@ local x, y = EntityGetTransform( GetUpdatedEntityID() )
 local currbiome = BiomeMapGetName( x, y )
 local flag = "toolbox_" .. tostring(currbiome) .. "_visited"
 
-if not GameHasFlagRun( flag ) then
+local prev_hm_count = tonumber( GlobalsGetValue( "D2D_TOOLBOX_PREV_HM_COUNT", "0" ) )
+local curr_hm_count = tonumber( GlobalsGetValue( "HOLY_MOUNTAIN_VISITS", "0" ) )
+
+local is_new_hm = curr_hm_count > prev_hm_count
+if is_new_hm then
+	GlobalsSetValue( "D2D_TOOLBOX_PREV_HM_COUNT", tostring( curr_hm_count ) )
+end
+
+if not GameHasFlagRun( flag ) or ( is_new_hm and curr_hm_count > 1 ) then
 	if generate_random_toolbox_spells( 1, true ) then
 		GameAddFlagRun( flag )
 	end
