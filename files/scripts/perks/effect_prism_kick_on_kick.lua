940c9c2e7b273a24339a155ee31a417b68186d50
Daniel Meyer <dansebmey@gmail.com>
2026-04-24 11:01:46 +0200

    cancel Prism Boot cast if there's a prop nearby

--- files/scripts/perks/effect_prism_kick_on_kick.lua
+++ files/scripts/perks/effect_prism_kick_on_kick.lua
@@ -1,9 +1,17 @@
 dofile_once( "mods/D2DContentPack/files/scripts/d2d_utils.lua" )
 
+function is_facing_prop( player )
+	local x, y = EntityGetTransform( player )
+	local props = EntityGetInRadiusWithTag( x, y, 13, "prop" )
+	return exists( props ) and #props > 0
+end
+
 function kick( entity_who_kicked )
+	if is_facing_prop( entity_who_kicked ) then return end
+
 	local pickup_count = get_perk_pickup_count( "D2D_PRISM_KICK" )
 
-	local children = EntityGetAllChildren( get_player() )
+	local children = EntityGetAllChildren( entity_who_kicked )
 	for k=1,#children do
 		child = children[k]
 	    if EntityGetName( child ) == "inventory_full" then
@@ -27,7 +35,7 @@ function kick( entity_who_kicked )
 							local mx, my = DEBUG_GetMouseWorld()
 							local dir_x = mx - ox
 							local dir_y = my - oy
-							shoot_projectile( get_player(), proj_path, ox, oy, dir_x, dir_y )
+							shoot_projectile( entity_who_kicked, proj_path, ox, oy, dir_x, dir_y )
 
 							-- maybe save a use if the player has more than 1 stack of this perk
 							local chance_to_spare_use = 0
@@ -37,7 +45,7 @@ function kick( entity_who_kicked )
 								else
 									chance_to_spare_use = 50
 								end
-							elseif pickup_count == 3 then
+							elseif pickup_count >= 3 then
 								if data.never_unlimited then
 									chance_to_spare_use = 75
 								else
@@ -48,7 +56,6 @@ function kick( entity_who_kicked )
 							SetRandomSeed( GameGetFrameNum(), GameGetFrameNum() )
 							local rnd = Random( 1, 100 )
 							if uses_remaining > 0 then
-								GamePrint( rnd .. " <= " .. chance_to_spare_use )
 								if rnd <= chance_to_spare_use then
 									local x, y = EntityGetTransform( GetUpdatedEntityID() )
 									GamePlaySound( "data/audio/Desktop/misc.bank", "game_effect/regeneration/tick", x, y )
