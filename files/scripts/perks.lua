583a3d7d3c1f196311f259d13e6ff6f077e2cf68
Daniel Meyer <dansebmey@gmail.com>
2026-04-28 20:26:52 +0200

    discontinue Fragile Heart

--- files/scripts/perks.lua
+++ files/scripts/perks.lua
@@ -1073,6 +1073,36 @@ d2d_curses = {
 
 	},
 
+	-- {
+	-- 	id = "D2D_CURSE_FRAGILE",
+	-- 	ui_name = "$perk_d2d_curse_fragile_name",
+	-- 	ui_description = "$perk_d2d_curse_fragile_desc",
+	-- 	ui_icon = "mods/D2DContentPack/files/gfx/ui_gfx/perks/curses/fragile_016.png",
+	-- 	perk_icon = "mods/D2DContentPack/files/gfx/ui_gfx/perks/curses/fragile.png",
+	-- 	stackable = STACKABLE_NO,
+	-- 	one_off_effect = false,
+	-- 	usable_by_enemies = false,
+	-- 	not_in_default_perk_pool = true,
+	-- 	func = function( entity_perk_item, entity_who_picked, item_name, pickup_count )
+	-- 		if reflecting then return end
+	-- 		local curse_count = GlobalsGetValue( "PLAYER_CURSE_COUNT", "0" )
+	-- 		GlobalsSetValue( "PLAYER_CURSE_COUNT", curse_count + 1 )
+
+	-- 		if pickup_count <= 1 then
+	-- 			EntityAddComponent2( entity_who_picked, "LuaComponent", 
+	-- 			{
+	-- 				_tags = "perk_component,d2d_curse_fragile",
+	-- 				script_damage_received = "mods/D2DContentPack/files/scripts/perks/effect_curse_fragile_on_damage.lua",
+	-- 				execute_every_n_frame = -1,
+	-- 			} )
+	-- 		end
+    --     end,
+    --     func_remove = function( entity_who_picked )
+    --     	dofile_once( "mods/D2DContentPack/files/scripts/d2d_utils.lua" )
+    --     	remove_lua( entity_who_picked, "d2d_curse_fragile" )
+    --     end
+	-- },
+
 	{
 		id = "D2D_CURSE_VOLATILE_PROPS",
 		ui_name = "$perk_d2d_curse_volatile_props_name",
