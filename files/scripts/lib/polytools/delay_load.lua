bfa2e3d82a8922beb21588f3d134ee0d933c7136
Daniel Meyer <dansebmey@gmail.com>
2026-05-03 23:58:11 +0200

    delete polytools again

--- files/scripts/lib/polytools/delay_load.lua
+++
@@ -1,4 +0,0 @@
-dofile_once("mods/D2DContentPack/files/scripts/lib/polytools/disco_util/disco_util.lua")
-local polytools = dofile_once("mods/D2DContentPack/files/scripts/lib/polytools/polytools.lua")
-local self = Entity.Current()
-polytools.load(self, self.var_str.polydata)
--- files/scripts/lib/polytools/fix_projectiles.lua
+++
@@ -1,4 +0,0 @@
-dofile_once("mods/D2DContentPack/files/scripts/lib/polytools/disco_util/disco_util.lua")
-
-local self = Entity.Current()
-if self.ProjectileComponent then self.ProjectileComponent.on_death_explode = true end
