2d613d004dae06adbd31054417358bca86d0950a
Daniel Meyer <dansebmey@gmail.com>
2026-05-05 12:00:07 +0200

    make parasite leave its host at 1 hp

--- files/entities/animals/parasite/parasite_status_shield_scan.lua
+++ files/entities/animals/parasite/parasite_status_shield_scan.lua
@@ -4,4 +4,12 @@ local host_id = EntityGetParent( GetUpdatedEntityID() )
 if not exists( host_id ) then return end
 local x, y = EntityGetTransform( host_id )
 
-EntityInflictDamage( host_id, 0.04, "NONE", "parasiitti", "NONE", 0, 0, host_id, x, y, 0 )
+local dmg_comp = EntityGetFirstComponentIncludingDisabled( host_id, "DamageModelComponent" )
+if exists( dmg_comp ) then
+	local hp = ComponentGetValue2( dmg_comp, "hp" )
+	local max_hp = ComponentGetValue2( dmg_comp, "max_hp" )
+
+	if hp > 0.04 then
+		EntityInflictDamage( host_id, 0.04, "NONE", "parasiitti", "NONE", 0, 0, host_id, x, y, 0 )
+	end
+end
